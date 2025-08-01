455a6506d47183436a260295bb888b4d67db2951
yuhsuansia <51372349+yuhsuansia@users.noreply.github.com>
2025-08-01 07:54:27 +0000

    Deploying to gh-pages from @ yuhsuansia/yuhsuansia.github.io@1c8bc760c49e44bc6cfa6341cea5614494f02b22 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -476,13 +476,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/yuxuan-xia-b2186b104", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
